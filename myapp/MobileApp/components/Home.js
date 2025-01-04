ab572ba57d36dd0ac1330340e8255d5219e2f09b
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-01-04 06:49:57 +0000

    Use Header for app

--- myapp/MobileApp/components/Home.js
+++ myapp/MobileApp/components/Home.js
@@ -1,4 +1,10 @@
 import { useNavigation } from '@react-navigation/native';
+import { PaperProvider } from 'react-native-paper';
 export default function Home() {
-    
+    <PaperProvider>
+        <View>
+             <Text variant="headlineLarge">Headline Large</Text>   
+             <Divider/>
+        </View>
+    </PaperProvider>
 }
