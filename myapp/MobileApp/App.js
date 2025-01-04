c74445f513649deed16211e8f2aa7b00bd0ec4ed
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-01-04 09:25:37 +0000

    Use NavigationContainer

--- myapp/MobileApp/App.js
+++ myapp/MobileApp/App.js
@@ -7,10 +7,10 @@ import AboutUs from './components/AboutUs';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+     <NavigationContainer>
+    </NavigationContainer> 
+    </SafeAreaProvider>
   );
 }
 
