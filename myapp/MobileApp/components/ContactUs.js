a80f8b1557844ee264b51426f5ac37b2808356c4
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-01-04 06:59:38 +0000

    Use text variant for headline

--- myapp/MobileApp/components/ContactUs.js
+++ myapp/MobileApp/components/ContactUs.js
@@ -1,4 +1,13 @@
 import { useNavigation } from '@react-navigation/native';
+import { PaperProvider } from 'react-native-paper';
 export default function ContactUs() {
-    
+    return(
+        <PaperProvider>
+           <View style={styles.header}>
+                <Text variant="headlineLarge">Contact Us</Text>
+                <Divider />
+            </View> 
+        </PaperProvider>
+
+    );
 }
