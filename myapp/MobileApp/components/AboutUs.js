a84708abc9fab5005aced18b81179cb6a5cca89a
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-01-04 07:20:48 +0000

    Added image source

--- myapp/MobileApp/components/AboutUs.js
+++ myapp/MobileApp/components/AboutUs.js
@@ -1,1 +1,15 @@
 import { useNavigation } from '@react-navigation/native';
+import { PaperProvider } from 'react-native-paper';
+export default function AboutUs() {
+    return(
+        <PaperProvider>
+           <View style={styles.container}>
+                <Text variant="headlineLarge">History</Text>
+                <Divider />
+                <View style={styles.imagepad}>
+                        <Image source={require('../assets/uovlogo.png')} style={styles.image}/>
+                </View>
+            </View>         
+        </PaperProvider>
+    );
+}
